ac7a4606de0706641ffc393cd9ed9927fd211970
Brian Kim <brian.kim@hardkernel.com>
2016-12-15 18:42:06 +0900

    c2play: add c2play version
    This change adds the c2play version in order to support various type of
    vertions of c2play. This is effectively to support not only ODROID-C2
    but also ODROID-C1+/C0 from each branch in c2play repository.
    
    Signed-off-by: Brian Kim <brian.kim@hardkernel.com>

--- package/c2play/c2play.mk
+++ package/c2play/c2play.mk
@@ -3,21 +3,28 @@
 # C2PLAY
 #
 #############################################################
-C2PLAY_VERSION = 1.0
-C2PLAY_SOURCE = master.zip
+C2PLAY_VERSION = $(call qstrip,$(BR2_PACKAGE_C2PLAY_VERSION))
+C2PLAY_SOURCE = $(C2PLAY_VERSION).zip
 C2PLAY_SITE = https://github.com/OtherCrashOverride/c2play/archive
-C2PLAY_DEPENDENCIES = alsa-lib aml_libs ffmpeg
+C2PLAY_DEPENDENCIES = alsa-lib aml_libs ffmpeg libass
 
 define C2PLAY_EXTRACT_CMDS
 	$(UNZIP) -d $(@D) $(DL_DIR)/$(C2PLAY_SOURCE)
-	mv $(@D)/c2play-master/* $(@D)
-	$(RM) -r $(@D)/c2play-master
+	mv $(@D)/c2play-$(BR2_PACKAGE_C2PLAY_VERSION)/* $(@D)
+	$(RM) -r $(@D)/c2play-$(BR2_PACKAGE_C2PLAY_VERSION)
 endef
 
+ifeq ($(C2PLAY_VERSION),S805)
+define C2PLAY_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) CC=$(TARGET_CXX) CXX=$(TARGET_CXX) -C $(@D) c2play
+	$(TARGET_MAKE_ENV) cp $(@D)/c2play $(TARGET_DIR)/usr/bin/
+endef
+else
 define C2PLAY_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) CC=$(TARGET_CXX) CFLAGS='$(TARGET_CFLAGS) -MMD -MF .$(@F).d -std=c++11' -C $(@D) all
 	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) install
 endef
+endif
 
 define C2PLAY_CLEAN_CMDS
 	$(MAKE) -C $(@D) clean
